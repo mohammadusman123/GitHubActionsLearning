cc660f2844728c8255cba1b834377a3b21f7b30f
Usman <usman.iftakhar@combinostics.com>
2024-02-06 15:00:58 +0200

    Add day of the week to show

--- GitHubActionsTraining/Program.cs
+++ GitHubActionsTraining/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/time/utc", () => Results.Ok("Current time is: " + DateTimeOffset.Now));
+app.MapGet("/time/utc",
+    () => Results.Ok("Current time is: " + DateTimeOffset.Now + " and days is: " +
+                     DateTimeOffset.Now.DayOfWeek));
 
 app.Run();
