cf8552a215baab46ed49e423857bfde913e01292
Usman <usman.iftakhar@combinostics.com>
2024-02-06 15:06:56 +0200

    Fix spellings

--- GitHubActionsTraining/Program.cs
+++ GitHubActionsTraining/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/time/utc",
-    () => Results.Ok("Current time is: " + DateTimeOffset.Now + " and days is: " +
+    () => Results.Ok("Current time is: " + DateTimeOffset.Now + " and the day is: " +
                      DateTimeOffset.Now.DayOfWeek));
 
 app.Run();
