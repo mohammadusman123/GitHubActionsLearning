8cc6b3c48746bd4c4876f638e051d2ed70dc589f
Usman <usman.iftakhar@combinostics.com>
2024-02-06 13:41:22 +0200

    Add Current time label

--- GitHubActionsTraining/Program.cs
+++ GitHubActionsTraining/Program.cs
@@ -3,6 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/time/utc", () => Results.Ok(DateTimeOffset.Now));
+app.MapGet("/time/utc", () => Results.Ok("Current time is: " + DateTimeOffset.Now));
 
 app.Run();
