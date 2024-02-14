4bfa287dbb157b05cacd4d5f250db5c027929fa5
Usman <usman.iftakhar@combinostics.com>
2024-02-14 15:32:40 +0200

    Add weather text

--- GitHubActionsTraining/Program.cs
+++ GitHubActionsTraining/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/time/utc",
     () => Results.Ok("Current time is: " + DateTimeOffset.Now + " and the day is: " +
-                     DateTimeOffset.Now.DayOfWeek));
+                     DateTimeOffset.Now.DayOfWeek + " and the weather is: look outside the window :p"));
 
 app.Run();
+
+public partial class Program
+{
+}
