f5dc1fab84342f3491a9b2a1b1caa0163f626542
Usman <usman.iftakhar@combinostics.com>
2024-04-23 11:24:32 +0300

    Update text

--- GitHubActionsTraining/Program.cs
+++ GitHubActionsTraining/Program.cs
@@ -6,7 +6,7 @@ var app = builder.Build();
 
 app.MapGet("/time/utc",
     () => Results.Ok("Current time is: " + DateTimeOffset.Now + ", day is: " +
-                     DateTimeOffset.Now.DayOfWeek + " and the weather is: look outside the window :p"));
+                     DateTimeOffset.Now.DayOfWeek + " and the weather is chilly"));
 
 app.Run();
 
