d1a9ac07eb4d21af3e79b18b425287dd09afd1b7
Usman <usman.iftakhar@combinostics.com>
2024-04-25 17:02:31 +0300

    Add test project

---
+++ GitHubActionsTraining.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace GitHubActionsTraining.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task ResponseNotEmpty()
+    {
+        //arrange
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        //act
+        var result = await client.GetStringAsync("/time/utc");
+
+        //assert
+        Assert.True(result.Length > 0);
+    }
+    
+    [Fact]
+    public async Task ResponseContainsCurrentDay()
+    {
+        //arrange
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        //act
+        var result = await client.GetStringAsync("/time/utc");
+
+        //assert
+        Assert.Contains("day", result.ToLower());
+    }
+}
